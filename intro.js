55380c1d9096cf283b8cb15416b83cc84625fd53
NaomiWambuiN <160010973+NaomiWambuiN@users.noreply.github.com>
2024-05-14 10:33:02 +0300

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,32 @@
+var x = "Mitchelle  and";
+var y = " Naomi";
+var w = "they are friends <br>";
+//document.write("My name is" + " " + x);
+z = x + y + w;
+document.write(z);
+//an integer or a number cannot be put in quotes since it will be treated as a string
+var a = "10";
+var b = "5 <br>";
+c = a + b;
+document.write(c);
+//correct one
+var aa = 10;
+var bb = 5;
+cc = aa + bb;
+document.write(cc);
+//how to access elements
+document.getElementById(Trial).innerHTML = 30;
+//how to input an alert
+//how to input alert
+// alert("welcome to Javascript😎");
+//x=5 assigning a variable x==5
+let w = 5;
+let q = 3;
+r = w % q;
+document.write(r);
+//increment
+let f = 7;
+document.write(f, f++);
+//exponation /raised to power **
+let g = 5;
+document.write(r ** 3);
