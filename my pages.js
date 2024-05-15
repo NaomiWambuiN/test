8b7ce87c97f9113379d6fe7b5f2a30def8d4b57e
NaomiWambuiN <160010973+NaomiWambuiN@users.noreply.github.com>
2024-05-15 13:30:43 +0300

    Add files via upload

---
+++ my pages.js
@@ -0,0 +1,6 @@
+function allnames() {
+    var a = prompt("Enter your first name here");
+    var b = prompt("Enter your second name");
+    var c=prompt("Enter your username")
+    document.write( c+"You've successfully signed up "   );
+  }
